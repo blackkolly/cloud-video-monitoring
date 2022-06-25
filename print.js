cee273cf6bd9196d4bb1fcafdd514c28b227a874
Oladipupo kola <komolafe.solomon@yahoo.com>
2022-06-25 15:25:16 +0100

    Print_state

---
+++ print.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+const name = "Oladipupo Kola"
+const height = "38";
+const country = "Nigeria";
+console.log(name)
+console.log(height)
+console.log(country)
